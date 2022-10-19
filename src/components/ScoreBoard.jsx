91dd48500be9efed32862812cc5f3535f8d4142b
FraterSKS <jackson.a.furr@gmail.com>
2022-10-18 23:10:47 -0400

    added results func

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,5 +1,7 @@
 const ScoreBoard = (props) => {
   const content = [];
+  let numCorrect = 0;
+  let numWrong = 0;
 
   let finalScore = parseInt((props.score / props.results.length) * 100);
 
@@ -9,10 +11,11 @@ const ScoreBoard = (props) => {
 
   for (let r of props.results) {
     if (r.correct === false) {
-      content.push({
-        ...r,
-        question: `${r.param1} X ${r.param2}`,
-      });
+      content.push(r);
+      numWrong++;
+    } else if (r.correct === true) {
+      content.push(r);
+      numCorrect++;
     }
   }
 
@@ -23,16 +26,26 @@ const ScoreBoard = (props) => {
           <div className="card-text">
             Final Score: {finalScore}%
             <br />
+            Correct: {numCorrect}/{props.results.length}
+            <br />
+            Wrong: {numWrong}/{props.results.length}
           </div>
         </div>
       </div>
 
       {content.map((el) => {
         return (
-          <div key={el.id} className="card resultsCard">
+          <div
+            key={el.id}
+            className={`card resultsCard ${el.correct ? "correct" : "wrong"}`}
+          >
             <div className="card-body">
+              <h5 className="card-title">
+                Question {el.id + 1}
+                <hr />
+              </h5>
               <div className="card-text">
-                {el.question}
+                {el.param1} X {el.param2}
                 <br />
               </div>
               <div className="card-text">
