47d1cb5d5918c4a570d3ae419fc9e46b0e4e1b31
FraterSKS <jackson.a.furr@gmail.com>
2022-10-18 22:30:46 -0400

    range form styling

--- src/components/GameLimits.jsx
+++ src/components/GameLimits.jsx
@@ -4,44 +4,77 @@ const GameLimits = (props) => {
   return (
     <Fragment>
       <div id="gameLimitCard">
-        Range:{" "}
-        <input
-          value={props.lowerLimit}
-          onChange={props.lowerLimitInputHandler}
-          type="number"
-          name="lowerRange"
-          className="form-control text-center gameLimitsForm"
-          required
-        />{" "}
-        to{" "}
-        <input
-          value={props.upperLimit}
-          onChange={props.upperLimitInputHandler}
-          type="number"
-          name="upperRange"
-          className="form-control text-center gameLimitsForm mb-3"
-          required
-        />
-        Time Limit (s):{" "}
-        <input
-          onChange={props.timeLimitInputHandler}
-          value={props.timeLimit}
-          type="number"
-          name="timeLimit"
-          className="form-control text-center gameLimitsForm mb-3"
-          required
-        />
-        Number Of Questions:{" "}
-        <input
-          onChange={props.numLimitInputHandler}
-          value={props.numLimit}
-          type="number"
-          name="numLimit"
-          className="form-control text-center gameLimitsForm"
-          required
-        />
-        <br />
-        <button onClick={props.startQuiz} className="mt-3 btn btn-success">
+        <div className="row">
+          <div className="col-4">
+            <div id="rangeCard1">
+              <div>
+                Range:{" "}
+                <input
+                  value={props.lowerLimit}
+                  onChange={props.lowerLimitInputHandler}
+                  type="number"
+                  name="lowerRange"
+                  className="text-center gameLimitsForm"
+                  required
+                />{" "}
+                to{" "}
+                <input
+                  value={props.upperLimit}
+                  onChange={props.upperLimitInputHandler}
+                  type="number"
+                  name="upperRange"
+                  className="text-center gameLimitsForm mb-3"
+                  required
+                />
+              </div>
+              <div>
+                Time Limit (s):{" "}
+                <input
+                  onChange={props.timeLimitInputHandler}
+                  value={props.timeLimit}
+                  type="number"
+                  name="timeLimit"
+                  className="text-center gameLimitsForm mb-3"
+                  required
+                />
+              </div>
+              <div>
+                Number Of Questions:{" "}
+                <input
+                  onChange={props.numLimitInputHandler}
+                  value={props.numLimit}
+                  type="number"
+                  name="numLimit"
+                  className="text-center gameLimitsForm"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+              cum iste asperiores. Consequatur quia nihil velit. Ipsa amet
+              laudantium voluptates, ipsum odit quam officia, dolore, reiciendis
+              dolorem hic repudiandae expedita. ol
+            </p>
+          </div>
+          <div className="col-4">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+              cum iste asperiores. Consequatur quia nihil velit. Ipsa amet
+              laudantium voluptates, ipsum odit quam officia, dolore, reiciendis
+              dolorem hic repudiandae expedita. ol
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button
+          onClick={props.startQuiz}
+          id="startBtn"
+          className="mt-3 btn btn-success"
+        >
           Start
         </button>
       </div>
